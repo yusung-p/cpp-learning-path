52adaebb293ed246db72793674b59731d71b447a
yusung-p <2724065943@qq.com>
2026-03-15 16:17:40 +0800

    feat：完善deque的功能，并与vector对比

--- 02_deque.cpp
+++ 02_deque.cpp
@@ -7,8 +7,12 @@ int main(){
     //头尾插入
     dq.push_back(10);
     dq.push_back(20);
-    dq.push_front(5);
+    dq.push_front(5);//vector是队列先进先出，只能从后端插入和删除，deque是双端队列，可以在头尾插入和删除
     dq.push_front(1);
+    //中间插入
+    dq.insert(dq.begin()+2,15);//在索引2的位置插入15，中间插入删除效率略优于vector，因为deque底层是分段连续的内存，而vector是连续的内存，插入删除需要移动元素
+    //删除中间元素
+    dq.erase(dq.begin()+3);//删除索引3的元素
     //遍历
     cout <<"deque元素：";
     for(int num:dq){
@@ -18,6 +22,9 @@ int main(){
     //访问元素
     cout<<"第一个元素："<<dq.front()<<endl;
     cout<<"最后一个元素："<<dq.back()<<endl;
+    cout<<"索引1的元素："<<dq[1]<<endl;
+    //deque长度
+    cout<<"deque长度："<<dq.size()<<endl;
     //删除头尾
     dq.pop_front();
     dq.pop_back();
@@ -26,5 +33,8 @@ int main(){
         cout<<num<<" ";
     }
     cout<<endl;
+    //清空deque
+    dq.clear();
+    cout<<"清空后deque长度："<<dq.size()<<endl;
     return 0;
 }
